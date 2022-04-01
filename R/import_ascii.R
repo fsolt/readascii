2214df8a033d92bd75972ddab7f6619404031e33
Frederick Solt <fsolt@mac.com>
2022-04-01 14:24:00 -0500

    initial commit (works for single-card files?) @hey-ikon @Tyhcass @sammo3182 @byngdeuk

---
+++ R/import_ascii.R
@@ -0,0 +1,194 @@
+#' Import ASCII datasets using SPSS syntax files
+#'
+#' \code{import_ascii} imports ASCII data files using their associated SPSS setup files.
+#'
+#' @param data_file A path to an ASCII data file.
+#' @param setup_file A path to the corresponding SPSS setup file.
+#' @param total_cards For multicard files, the number of cards in the file.
+#' @param var_names A string vector of variable names.
+#' @param var_cards For multicard files, a numeric vector of the cards on which \code{var_names} are recorded.
+#' @param var_positions A numeric vector of the column positions in which \code{var_names} are recorded.
+#' @param var_widths A numeric vector of the widths used to record \code{var_names}.
+#' @param card_pattern For use when the file does not contain a line for every card for every respondent (or contains extra lines that correspond to no respondent), a regular expression that matches the file's card identifier; e.g., if the card number is stored in the last digit of each line, "\\d$".
+#' @param respondent_pattern For use when the file does not contain a line for every card for every respondent (or contains extra lines that correspond to no respondent), a regular expression that matches the file's respondent identifier; e.g., if the respondent number is stored in the first four digits of each line, preceded by a space, "(?<=^\\s)\\d{4}".
+#'
+#' @details Many older survey datasets are available only in ASCII format, which is notoriously difficult to work with.  The `import_ascii` function imports uses the SPSS setup file to import data stored in this archaic format.
+#'
+#' See \code{\link[readroper]{read_rpr}} and \code{\link[ropercenter]{read_roper}} for implementations that read single, specified variables.
+#'
+#' @return A data frame
+#'
+#' @examples
+#'
+#' @importFrom readr read_lines parse_guess
+#' @importFrom tibble as_tibble
+#' @importFrom purrr as_vector
+#' @importFrom dplyr '%>%' mutate filter select group_by summarise if_else row_number first
+#' @importFrom tidyr pivot_longer spread fill separate nest
+#' @importFrom stringr str_extract str_replace str_replace_all str_detect str_trim str_c
+#' @importFrom labelled val_labels set_variable_labels
+#'
+#' @export
+import_ascii <- function(data_file,
+                         setup_file,
+                         total_cards = 1,
+                         var_names,
+                         var_cards = 1,
+                         var_positions,
+                         var_widths,
+                         card_pattern,
+                         respondent_pattern) {
+
+  setup <- read_lines(setup_file) %>%
+    as_tibble() %>%
+    mutate(section = str_extract(value, "^\\s*(?<!\\/\\*)[^\\*]*((DATA LIST)|(VARIABLE LABELS)|(VALUE LABELS))")) %>%
+    fill(section)
+
+  data_list <- setup %>%
+    filter(str_detect(value, "\\w+\\s\\d+-\\d+")) %>%
+    mutate(value = str_trim(value)) %>%
+    separate(value,
+             sep = "\\s+(?=\\w+\\s\\d+-\\d+)",
+             into = LETTERS[1:5],
+             fill = "right") %>%
+    pivot_longer(LETTERS[1:5]) %>%
+    filter(!is.na(value)) %>%
+    separate(value,
+             sep = "[\\s-]+",
+             into = c("variable", "first_col", "last_col", "notes"),
+             fill = "right") %>%
+    mutate(first_col = as.numeric(first_col),
+           last_col = as.numeric(last_col),
+           width = last_col - first_col + 1,
+           card = 1) %>%
+    select(variable, first_col, last_col, width, card)
+
+  variable_labels <- setup %>%
+    filter(str_detect(section, "LABELS")) %>%
+    mutate(section = str_extract(value, ".*LABELS.*")) %>%
+    fill(section) %>%
+    filter(str_detect(section, "VARIABLE")) %>%
+    mutate(var = str_extract(value, "^[^'\"]*") %>%
+             str_trim(),
+           label = str_extract(value, "\".*\""),
+           label2 = if_else(is.na(label), str_extract(value, "'.*'"), label) %>%
+             str_replace_all("^[\"']|[\"']$", "") %>%
+             str_trim(),
+           id = if_else(var != "", row_number(), NA_integer_)) %>%
+    fill(id) %>%
+    group_by(id) %>%
+    summarise(variable = first(var),
+              label = str_c(label2, collapse = " ")) %>%
+    filter(!is.na(label)) %>%
+    select(-id)
+
+  value_labels <- setup %>%
+    filter(str_detect(section, "LABELS")) %>%
+    mutate(section = str_extract(value, ".*LABELS.*")) %>%
+    fill(section) %>%
+    filter(str_detect(section, "VALUE")) %>%
+    mutate(variable = if_else(str_detect(value, "^\\s*[\\d\"\\']"), NA_character_, str_trim(str_extract(value, "(^\\s*[\\d\"\\'].*\\s+)\\w+"))),
+           leftover = if_else(str_detect(value, "^\\s*[\\d\"\\']"), str_trim(str_replace(value, "\\w+(.*)", "\\1")), NA_character_),
+           value2 = if_else(is.na(variable),
+                            str_replace(value, "((\\d+)|(\".\")|(\\'.\\'))\\s.*", "\\1") %>%
+                              str_trim() %>%
+                              str_replace("/", ""),
+                            str_replace(leftover, "((\\d+)|(\".\")|(\\'.\\'))\\s.*", "\\1") %>%
+                              str_trim() %>%
+                              str_replace("/", "")),
+           label2 = if_else(is.na(variable),
+                            str_replace(value, paste0("\\s*", value2, "\\s.*(.*)"), "\\1") %>%
+                              str_replace("/", "") %>%
+                              str_trim(),
+                            NA_character_)) %>%
+    fill(variable) %>%
+    filter(!is.na(value2)) %>%
+    select(variable, value2, label2) %>%
+    nest(labels = c(value2, label2))
+
+  var_labels <- split(variable_labels$label, variable_labels$variable)
+
+  ascii_dataset <- read_ascii(data_file,
+                              var_names = data_list$variable,
+                              var_positions = data_list$first_col,
+                              var_widths = data_list$width,
+                              total_cards = total_cards,
+                              var_cards = data_list$card,
+                              card_pattern = card_pattern,
+                              respondent_pattern = respondent_pattern)
+
+  ascii_dataset2 <- ascii_dataset %>%
+    labelled::set_variable_labels(.labels = var_labels) %>%
+    select(-respondent, -matches("card\\d+"))
+
+  try(
+    for(i in 1:nrow(value_labels)) {
+    x_labs <- value_labels[i, "labels"][[1]][[1]]
+    labelled::val_labels(ascii_dataset2[, value_labels$variable[i]]) <- split(as.numeric(x_labs$value2), x_labs$label2) %>%
+      as_vector()
+    },
+  silent = TRUE)
+
+  return(ascii_dataset2)
+}
+
+
+read_ascii <- function(file,
+                       total_cards = 1,
+                       var_names,
+                       var_cards = 1,
+                       var_positions,
+                       var_widths,
+                       card_pattern,
+                       respondent_pattern) {
+
+  . <- value <- NULL   # satisfy R CMD check
+
+  if ((length(read_lines(file)) %% total_cards) != 0 & (missing(card_pattern) | missing(respondent_pattern))) {
+    stop("The number of lines in the file is not a multiple of the number of cards in the file.  Please specify card_pattern and respondent_pattern", call. = FALSE)
+  }
+
+  if (length(var_cards) == 1 & !missing(var_names)) {
+    var_cards = rep(var_cards, length(var_names))
+  }
+
+  if (missing(card_pattern)) {
+    df <- read_lines(file) %>%
+      as_tibble() %>%
+      mutate(card = paste0("card", rep_len(seq_len(total_cards), nrow(.))),
+             respondent = rep(seq(to = nrow(.)/total_cards), each = total_cards)) %>%
+      spread(key = "card", value = "value")
+  } else {
+    df <- read_lines(file) %>%
+      as_tibble() %>%
+      mutate(card = paste0("card", str_extract(value, card_pattern))) %>%
+      filter(!card == "cardNA") %>%
+      mutate(respondent = str_extract(value, respondent_pattern)) %>%
+      spread(key = "card", value = "value")
+  }
+
+  if (!missing(var_names)) {
+    if (missing(var_positions) | missing(var_widths)) {
+      stop("Variable positions and widths should also be given when variable names are specified", call. = FALSE)
+    } else if (length(unique(sapply(list(var_names, var_positions, var_widths), length))) > 1) {
+      stop("The lengths of the vectors of variable names, positions, and widths must be the same", call. = FALSE)
+    } else if ((max(var_cards) > max(total_cards))) {
+      stop("When reading a multi-card dataset, the numbers of the cards with variables to be read must not be greater than the total number of cards",
+           call. = FALSE)
+    }
+
+    for (i in seq_along(var_names)) {
+      card <- paste0("card", var_cards[i])
+      df[[var_names[i]]] <- parse_guess(str_replace(df[[card]],
+                                                    paste0("^.{", var_positions[i] - 1, "}(.{", var_widths[i], "}).*"),
+                                                    "\\1") %>%
+                                          str_replace("^\\s+$", ""))
+      if (var_widths[i] < 4) {
+        is.na(df[[var_names[i]]]) <- which(!nchar(df[[var_names[i]]]) == var_widths[i])
+      }
+    }
+  }
+
+  return(df)
+}
+
